6e0c4e572769976aa880dbb49413019bd0fced78
Sinh Hoang <lehoangsinhhn99@gmail.com>
2022-09-05 15:17:11 +0700

    Initial Commit

---
+++ BuildPhraseScript/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BuildPhraseScript
+//
+//  Created by Lê Hoàng Sinh on 05/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
